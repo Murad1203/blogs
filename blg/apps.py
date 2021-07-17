38d70b19a43673a6ddd1a7ab46bdb001c5e154c1
Murad1203 <lutkun666@gmail.com>
2021-07-17 22:01:36 +0300

    first

---
+++ blg/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BlgConfig(AppConfig):
+    name = 'blg'
---
+++ blg/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class BlogApps(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    content = models.TextField(verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add = True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now = True, verbose_name='Обновлено')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блогг'
+        ordering = ['-created_at']
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+
+    def __str__(self):
+        return self.title
